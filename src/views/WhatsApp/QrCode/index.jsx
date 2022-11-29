0d7f2a8785e58cdf3d68c812b1d7280bfa6c29e1
Lucas Bueno <lucas_bueno2@estudante.sc.senai.br>
2022-11-29 00:55:09 -0300

    Websocket e sessões do WhatsApp funcionando!

--- src/views/WhatsApp/QrCode/index.jsx
+++ src/views/WhatsApp/QrCode/index.jsx
@@ -2,49 +2,71 @@ import React from "react";
 
 import "./style.css";
 
-import cuid from "cuid";
+import { useSessions } from "../queries";
 import { useParams } from "react-router-dom";
 import socket from "../../../utils/ws/websocket.config";
-import { useSessions } from "../queries";
 
 const QrCode = props => {
   let { id } = useParams();
-
+  const [qr, setQr] = React.useState("");
   const [log, setLog] = React.useState("");
+  const [message, setMessage] = React.useState("");
   const [isConnected, setIsConnected] = React.useState(socket.connected);
 
   const { data: sessions } = useSessions();
 
   React.useEffect(() => {
+    setQr("");
+    setLog("");
+    setMessage("");
+
+    sessions?.data.forEach(session => {
+      if (session.id !== id) {
+        socket.emit("close-client", session.id);
+      }
+    });
+
     socket.on("connect", () => {
       setIsConnected(true);
     });
 
-    sessions?.data.forEach(session => socket.emit("unsubscribe-session", session.id));
+    socket.emit("generate-session", { id: id });
 
-    socket.emit("join-session", id);
+    socket.on("message", message => {
+      setLog(message);
+    });
 
-    socket.emit("send_message", { id: id, message: cuid() });
+    socket.on("new_message", message => {
+      setMessage(message);
+    });
 
-    socket.on("message", data => {
-      setLog(data.message);
-      console.log(data.session);
-      socket.emit("message", { id: id, message: "Novo socket conectado!" });
+    socket.on("qr", url => {
+      setQr(url);
+    });
+
+    socket.on("chats", chats => {
+      console.log(chats);
     });
 
     return () => {
       socket.off("connect");
-      socket.off("join-session");
       socket.off("message");
+      socket.off("new_message");
+      socket.off("generate-session");
+      socket.off("close-client");
+      socket.off("chats");
+      socket.off("qr");
     };
-  }, [id]);
+  }, [id, sessions?.data]);
 
   return (
     <div className="w-100 h-100 d-flex flex-column justify-content-center align-center">
       <div className="d-flex flex-column justify-content-center align-center">
         <h3 className="text-color text-center mb-10">WhatsApp</h3>
         {isConnected && <p className="text-color text-center mb-20">WebSocket conectado</p>}
-        <p className="text-color text-center">{log}</p>
+        {qr && <img src={qr} alt="Qr-code gerado para conectar-se ao WhatsApp" className="mb-20" />}
+        <p className="text-color text-center mb-20">{log}</p>
+        <p className="text-color text-center">{message}</p>
       </div>
     </div>
   );
